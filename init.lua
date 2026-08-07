7d2eb3f2450524c5ebe03aeb2399f7c95f4898fc
Surzal-kali <vgreenwald@student.cnm.edu>
2026-08-06 20:10:22 -0600

    my vim

---
+++ init.lua
@@ -0,0 +1,19 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.cmd.colorscheme("tokyonight")
+
+vim.opt.clipboard = "unnamedplus" -- use system keyboard for yank
+vim.opt.autowriteall = true -- automatically write file when switching buffers
+vim.opt.nu = true -- set line numbers
+-- set tab size to 2 spaces
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.wrap = true
+
+vim.opt.incsearch = true -- incremental search
+
+vim.opt.termguicolors = true
